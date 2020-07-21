839c4f9c14bad3f79c5c7b31811aa09dc2d1cfdd
tgomez22 <gomez22@pdx.edu>
2020-07-21 12:05:26 -0700

    Working 5 day weather api call. I need to fix css formatting, and implement a method to accept coordinates.

--- src/Weather.js
+++ src/Weather.js
@@ -2,19 +2,22 @@ import React from 'react';
 import {Component} from 'react';
 import { Card } from 'react-bootstrap';
 import "./Weather.css";
-/*const url = Placeholder! */
-
+const url = 
 
 
 export default class Weather extends Component{
     constructor(props){
         super(props);
         this.state = {
-            WeatherDescription: "",
+            highs: [],
+            lows: [],
+            weatherDescription: [],
             key: url,
-            icon: "",
+            icon: [],
             error: null,
             isLoaded: false,
+            latitute: 45.521744, //placeholder for testing
+            longitude: -122.689947, //placeholder for testing
         };
     }
 
@@ -34,14 +37,61 @@ export default class Weather extends Component{
     <div className="row justify-content-center fixed-bottom">
         <div className="col-2 d-flex">
             <Card style={{width:'80%'}}>
-                <Card.Img src={this.state.icon}/>
+                <Card.Img src={this.state.icon[0]}/>
                 <Card.Title>Date Placeholder</Card.Title>
                     <Card.Text>
-                        {this.state.weatherDescription}
+                        {this.state.weatherDescription[0]}
+                        It is currently: {this.state.highs[0]} degrees out.
+                    </Card.Text>
+            </Card>
+        </div> 
+        <div className="col-2 d-flex" >
+            <Card style={{width:'80%'}}>
+                <Card.Img src={this.state.icon[1]}/>
+                <Card.Title>Date Placeholder</Card.Title>
+                    <Card.Text>
+                        {this.state.weatherDescription[1]}
+                        High: {this.state.highs[1]} degrees Fahrenheit.
+                        Low: {this.state.lows[0]} degrees Fahrenheit.
+                    </Card.Text>
+            </Card>
+        </div> 
+        <div className="col-2 d-flex" >
+            <Card style={{width:'80%'}}>
+                <Card.Img src={this.state.icon[2]}/>
+                <Card.Title>Date Placeholder</Card.Title>
+                    <Card.Text>
+                        {this.state.weatherDescription[2]}
+                        High: {this.state.highs[2]} degrees Fahrenheit.
+                        Low: {this.state.lows[1]} degrees Fahrenheit.
+                    </Card.Text>
+            </Card>
+        </div> 
+        <div className="col-2 d-flex" >
+            <Card style={{width:'80%'}}>
+                <Card.Img src={this.state.icon[3]}/>
+                <Card.Title>Date Placeholder</Card.Title>
+                    <Card.Text>
+                        {this.state.weatherDescription[3]}
+                        High: {this.state.highs[3]} degrees Fahrenheit.
+                        Low: {this.state.lows[2]} degrees Fahrenheit.
+                    </Card.Text>
+            </Card>
+        </div> 
+         <div className="col-2 d-flex">
+            <Card style={{width:'80%'}}>
+                <Card.Img src={this.state.icon[4]}/>
+                <Card.Title>Date Placeholder</Card.Title>
+                    <Card.Text>
+                        {this.state.weatherDescription[4]}
+                        High: {this.state.highs[4]} degrees Fahrenheit.
+                        Low: {this.state.lows[3]} degrees Fahrenheit.
                     </Card.Text>
             </Card>
         </div> 
     </div>
+    
+
             );
         }
     }
@@ -59,12 +109,41 @@ export default class Weather extends Component{
         .then(
             (jsonResponse) => 
         {
+            let icons = [];
+
+            icons.push("http://openweathermap.org/img/wn/" + jsonResponse.current.weather[0].icon + "@2x.png");
+            icons.push("http://openweathermap.org/img/wn/" + jsonResponse.daily[0].weather[0].icon + "@2x.png"); 
+            icons.push("http://openweathermap.org/img/wn/" + jsonResponse.daily[1].weather[0].icon + "@2x.png"); 
+            icons.push("http://openweathermap.org/img/wn/" + jsonResponse.daily[2].weather[0].icon + "@2x.png");
+            icons.push("http://openweathermap.org/img/wn/" + jsonResponse.daily[3].weather[0].icon + "@2x.png");
+
+            let descriptions = [];
+            descriptions.push(jsonResponse.current.weather[0].description);
+            descriptions.push(jsonResponse.daily[0].weather[0].description);
+            descriptions.push(jsonResponse.daily[1].weather[0].description);
+            descriptions.push(jsonResponse.daily[2].weather[0].description);
+            descriptions.push(jsonResponse.daily[3].weather[0].description);
+
+            let highTemps = [];
+            highTemps.push(jsonResponse.current.temp);
+            highTemps.push(jsonResponse.daily[0].temp.max);
+            highTemps.push(jsonResponse.daily[1].temp.max);
+            highTemps.push(jsonResponse.daily[2].temp.max);
+            highTemps.push(jsonResponse.daily[3].temp.max);
+
+            let lowTemps = [];
+            lowTemps.push(jsonResponse.daily[0].temp.min);
+            lowTemps.push(jsonResponse.daily[1].temp.min);
+            lowTemps.push(jsonResponse.daily[2].temp.min);
+            lowTemps.push(jsonResponse.daily[3].temp.min);
                 this.setState({
-                    weatherDescription:jsonResponse.weather[0].description,
+                    highs: highTemps,
+                    lows: lowTemps,
+                    weatherDescription: descriptions,
                     isLoaded:true,
                     error: false,
-                    icon: "http://openweathermap.org/img/wn/" + `${jsonResponse.weather[0].icon}` 
-        + "@2x.png"})
+                    icon: icons
+                })
         },
         (error) => {
             this.setState({
