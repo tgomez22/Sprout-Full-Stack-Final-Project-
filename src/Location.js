1f3e16e4cb813f88dfa3b25cb9cfae173123d243
tgomez22 <gomez22@pdx.edu>
2020-07-29 14:34:55 -0700

    Huge changes to the Location.js and Navigation.js

--- src/Location.js
+++ src/Location.js
@@ -1,69 +1,23 @@
 import React from 'react';
 import {Component} from 'react';
+import { Redirect } from 'react-router-dom';
 const GOOGLE_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
-//supplied test zip
-//const zipCode = "97206";
-//
-//The call used a supplied zip code for testing. 
-//const CALL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + zipCode +"&key=" + GOOGLE_KEY;
-
-//This call was used to turn lat/long into a zipcode.
-
-const CALL = "https://maps.googleapis.com/maps/api/geocode/json?latlng=45.48563720000001,-122.5946255&key=" + GOOGLE_KEY;
-
-
-export default class Location extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            isLoaded: false,
-            error: null, 
-            lat: 45.48563720000001, //preloaded coordinates
-            long: -122.5946255, //preloaded coordinates.
-            zip: null, 
-        };
-    }
-
-    render(){
-        if(this.state.error === null || this.state.error === true){
-            return(
-                <div className ='row'>
-                    <div className = 'col-12'>
-                        Nothing Loaded yet!
-                    </div>
-                </div>
-            );
-        } else {
-            return(
-                <div className = 'row'>
-                    <div className = 'col-12'>
-                       {this.state.zip}
-                    </div>
-                </div>
-            );
-        }
-    }
-
     //use if supplied lat/long
-    getZip(){
-        fetch(CALL)
+    export let getZip = async (latitude, longitude) => {
+        return fetch("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude.toString() + ',' + longitude.toString() + "&key=" + GOOGLE_KEY)
             .then(response => response.json())
             .then(jsonResponse => {
                 let data = jsonResponse;
-                console.log(data);
-                
-                this.setState({
-                    isLoaded: true,
-                    error: false,
-                    zip: data.results[0].address_components[7].long_name,
-                })
+                let location = [''];
+                location.concat(data.results[0].address_components[7].long_name);
+                location.concat(latitude);
+                location.concat(longitude);
+                return(location);
             },
             (error) => {
-                this.setState({
-                    error,
-                    isLoaded: false,
-                });
+               let err = null;
+               return (err);
             })
     }
 
@@ -71,35 +25,51 @@ export default class Location extends Component{
 
 
         //use if user supplied zip
-        getLatLong(){
-        fetch(CALL)
+    export let getLatLong = async (userZip) => {
+        if(userZip === undefined){
+            return null;
+        } 
+        return fetch("https://maps.googleapis.com/maps/api/geocode/json?address=" + userZip.toString() + 'components=postal_code:' + userZip.toString() +"&key=" + GOOGLE_KEY)
             .then(response => response.json())
             .then(jsonResponse => {
                 let data = jsonResponse;
+                
+                if(jsonResponse === undefined){
+                    window.alert("Error when retrieving location data");
+                    return(<Redirect to="/" />);
+                }
+               
 
-                //used for testing
-                //console.log(data);
-
-
-                this.setState({
-                    isLoaded: true,
-                    error: false,
-                    lat: data.results[0].geometry.location.lat,
-                    long: data.results[0].geometry.location.lng,
-
-                    //this is a supplied test zip.
-                    zip: data.results[0].address_components[0].long_name,
-                })
+                let location = [];
+                location.push(userZip);
+                location.push(data.results[0].geometry.location.lat);
+                location.push(data.results[0].geometry.location.lng);
+                return(location);
+            
 
             },
             (error) => {
-                this.setState({
-                    error,
-                    isLoaded: false,
-                });
+               console.log(error);
             }
-            )
+            );
     }
 
 
-}
+    export let useCoordinates = async (position) => {
+            return(getZip(position.coords.latitude, position.coords.longitude));
+   }
+
+    export let useMyZip = (userZip) => {
+        let location = getLatLong(userZip);
+        return(location);
+   }
+
+   export let error = () => {
+    console.log("Failed to get coordinates");
+   }
+
+   
+        
+
+
+
