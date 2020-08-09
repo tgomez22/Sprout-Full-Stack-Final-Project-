808ffb2b0685989d0cdb37f3892b62d1c7beca3c
unknown <hsiebert@pdx.edu>
2020-08-08 17:39:48 -0700

    nursery query fix attempt

--- src/Nursery.js
+++ src/Nursery.js
@@ -6,7 +6,7 @@ import PageButtons from "./pageButtons";
 const apiKey = process.env.REACT_APP_TREFLE_API_KEY;
 
 const proxyUrl = "https://cors-anywhere.herokuapp.com/"; //for testing purposes only
-const url = `https://trefle.io/api/v1/species/search?limit=8&token=${apiKey}&page=`;
+const url = `https://trefle.io/api/v1/species/search?q=&limit=8&token=${apiKey}&page=`;
 
 function Nursery() {
   const [finalURL, setFinalUrl] = useState(null);
